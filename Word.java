76894c2994b2494f9307d368f4c2e908665adf01
Matthew Gray <mrgray4@ncsu.edu>
2015-11-11 18:41:07 -0500

    Finished Structure of Word, began online definitions, and testing
    Added toString, equals, and an overload of getDefinition to Word.java.
    Tested Word.java with WordTest.java just getting a basic definition.
    Also added resources to txt resource file.

--- Word.java
+++ Word.java
@@ -1,8 +1,11 @@
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.util.Scanner;
 
 /**
- * Class representing a word and its definition to find.
+ * Class representing a word and 
+ * its definition to find.
  * @author Matthew Gray
  */
 public class Word
@@ -50,10 +53,32 @@ public class Word
         if (overwrite || (definition == null || definition.isEmpty())) {
             //get definition from online service
             //if not able to access internet, then do something
-            return "";
+            try {
+                InputStream response = new URL(SERVICE_URL + this.word).openStream();
+                Scanner responseReader = new Scanner(response).useDelimiter("\\A");
+                if (responseReader.hasNext()) {
+                    String responseString = responseReader.next();
+                    response.close();
+                    return responseString;
+                } else {
+                    return "Definition not found.";
+                }
+            } catch (Exception ex) {
+                return ex.toString(); //if this becomes a problem, handle better
+            }
         } else {
             return this.definition;
         }
+        return "Wait this should never be reached? Why do I need a return here?";
+    }
+
+    /**
+     * Gets the definition of a word, sets it if necessary, and returns it.
+     * @return String containint the definition of the word.
+     */
+    public String getDefinition()
+    {
+        return this.getDefinition(false);
     }
 
     /**
@@ -63,5 +88,44 @@ public class Word
     public void setDefinition(String definition)
     {
         this.definition = definition;
+    }
+
+    /**
+     * Gets the word itself.
+     * @return the string representing the word of this object.
+     */
+    public String getWord()
+    {
+        return this.word;
+    }
+
+    /**
+     * Returns a String representing this word and definition pair.
+     * @return the string representing this word and its definition.
+     */
+    public String toString()
+    {
+        return this.getWord() + this.getDefinition();
+    }
+
+    /**
+     * Comparison of this object with another object, potentially another word.
+     * If the definition of either word isn't set, then the comparison will be false.
+     * This was chosen to conserve resources/time when doing potential large batch
+     * comparisons.
+     * @param o the other object to compare to.
+     * @return whether this word is equal to another word, true if so, false if otherwise.
+     */
+    public boolean equals(Object o)
+    {
+        if (o instanceof Word) {
+            Word otherWord = (Word)o;
+            if (this.getWord().equals(otherWord.getWord())) {
+                if (this.definition.equals(otherWord.getDefinition())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ WordTest.java
@@ -0,0 +1,13 @@
+/**
+ * Tests for Word class.
+ * @author Matthew Gray
+ */
+public class WordTest
+{
+    public static void main(String[] args)
+    {
+        Word word = new Word("computer");
+        System.out.println(word.getDefinition());
+        System.out.println(word);
+    }
+}
